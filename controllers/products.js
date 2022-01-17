f757ade47e0369a47d5e1e1ce88521c1cefa3e87
FurkanBekereci <frknbkrci@gmail.com>
2022-01-17 21:59:08 +0300

    Filtering is coming to good step.

--- controllers/products.js
+++ controllers/products.js
@@ -5,8 +5,8 @@ module.exports.getProducts = async (req,res) => {
     const queryObject = generateQueryObject(req.query,Product);
     const sorterString = getSorterString(req.query);
     const selectorString = getSelectorString(req.query);
-    const limit = getLimit(req.query);
-    const skip = getSkip(req.query);
+    const { limit , skip } = getPage(req.query);
+    
     //Filtering, sorting and selecting
     const products = await Product.find(queryObject)
             .sort(sorterString)
@@ -36,51 +36,143 @@ module.exports.getProductStatic = async (req,res) => {
     res.status(200).json({nbHits: products.length, products : products})
 }
 
-
+//#region Filtering
 //Todo Burası genelleştirilecek.
 const generateQueryObject = (reqQuery,T) => {
+    const {filters} = reqQuery;
     let queryObject = {};
-    Object.keys(T.schema.obj).forEach(key => {
-        if(!!reqQuery[key]){
-            queryObject = {...queryObject, [key]: filterGeneratorAccordingToType(T.schema.obj[key].type, reqQuery[key])}
-        }
+    const modelList = Object.keys(T.schema.obj);
+    console.log("filter:", filters);
+    if(!filters) return {};
+    if(filters.lenght == 0) return {};
+    
+    
+    filters.forEach(filter => {
+        if(modelList.indexOf(filter['field']) == -1) return;
+        const generatedQuery = generateQueryFromFilter(filter);
+        if(!generatedQuery) return;
+
+        queryObject = {...queryObject, ...generatedQuery};
     });
+    
+    
+    console.log("queryObject:",queryObject);
 
     return queryObject;
 }
 
+// Object.keys(T.schema.obj).forEach(key => {
+    //     if(!!filter[key]){
+    //         const value = filterGeneratorAccordingToType(filter[key])
+    //         if(!!value)
+    //         queryObject = {...queryObject, ...value}
+    //     }
+    // });
 
-const filterGeneratorAccordingToType = (type, value) => {
-    switch(type) {
-        case String: 
-        return { $regex : value, $options : 'i'};
-        case Number : 
-        return { $lt : Number(value)};
-        case Boolean :
-        return value === 'true';
-        default :
-        return value;
+
+const generateQueryFromFilter = ({type = 'and',field,operator,value }) => {
+
+    console.log("type:",type);
+    console.log("field:",field);
+    console.log("operator:",operator);
+    console.log("value:",value);
+
+    if((Array.isArray(operator) && !Array.isArray(value)) || ((!Array.isArray(operator)) && Array.isArray(value)))
+        return undefined;
+
+    if(Array.isArray(operator) && Array.isArray(value)){
+        if(operator.length != value.length) return undefined;
+
+        const fieldQueryArray = [];
+        for (let i = 0; i < operator.length; i++) {
+            const op = operator[i];
+            const val = value[i];
+            const fieldQueryElement = mapQueryTypeWithOperatorAndValue(op,val) || {};
+            fieldQueryArray.push({[field]: {...fieldQueryElement}})
+        }
+        type = `$${type}`;
+        return { [type] : fieldQueryArray }
+
     }
+    else{
+        const singlefieldQueryElement = mapQueryTypeWithOperatorAndValue(operator,value);
+        return !!singlefieldQueryElement ? { [field] : singlefieldQueryElement } : undefined;
+    }
+      
 }
 
+const mapQueryTypeWithOperatorAndValue = (operator,value) => { 
+    switch (operator) {
+        case 'equals':
+            return value;
+        case 'notEquals':
+            return {'$ne' : value};
+        case 'greaterThan':
+            return {'$gt' : value};
+        case 'greaterThanOrEquals':
+            return {'$gte' : value};
+        case 'lessThan':
+            return {'$lt' : value};
+        case 'lessThanOrEquals':
+            return {'$lte' : value};
+        case 'in':
+            return {'$in' : value};
+        case 'notIn':
+            return {'$nin' : value};
+        case 'between':
+            return { '$lt' : value , '$gt' : value};
+        case 'betweenOrEquals':
+            return {'$lte' : value , '$gte' : value};
+        case 'startsWith':
+            return {'$regex' : `^${value}.*`};
+        case 'endsWith':
+            return { '$regex' : `${value}` + "$"};
+        case 'contains' : 
+            return { '$regex' : value };
+        case 'isTrue':
+            return value === 'true';
+        case 'isFalse':
+            return value === 'false';
+        case 'isEmpty':
+            return { '$exist' : true , '$size' : 0 };
+        case 'isNotEmpty':
+            return { '$exist' : true , '$not' :  { '$size' : 0}};
+        case 'isNull' : 
+            return { '$exist' : false};
+        case 'isNotNull' : 
+            return { '$exist' : true}
+        case 'isNullOrEmpty' : 
+            return { '$or' : [{'$exist' : false}, {'$exist' : true, '$size' : 0 }]}
 
-//Sorting
+        default:
+            return undefined;
+    }
+
+}
+
+//#endregion
+
+//#region Sorting
 const getSorterString = (reqQuery) => {
     const { sort } = reqQuery; 
     return sort?.toString().replace(/,/g,' ')
 }
+//#endregion
 
+//#region Select field
 const getSelectorString = (reqQuery) => {
     const { select } = reqQuery; 
     return select?.toString().replace(/,/g,' ')
 }
-const getLimit = (reqQuery) => {
-    const { limit } = reqQuery; 
-    return !!limit ? Number(limit) : undefined;
+//#endregion
+
+//#region Set page
+const getPage = (reqQuery) => {
+    const limit = Number(reqQuery.limit) || 200;
+    const page = Number(reqQuery.page) || 1;
+    const skip = (page - 1 ) * limit;
+    return { skip , limit}
 }
-const getSkip = (reqQuery) => {
-    const { skip } = reqQuery; 
-    return !!skip ? Number(skip) : undefined;
-}
+//#endregion
 
 //Video kalınan dakika 5:05:00 / 9:59:58
